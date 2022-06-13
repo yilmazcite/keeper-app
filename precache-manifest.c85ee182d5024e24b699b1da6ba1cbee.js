3722501df216c4d7a4e428e41c7348f1ff16dfe2
Yilmaz Cite <yilmazcite@gmail.com>
2022-06-13 19:28:40 +0300

    Updates

---
+++ precache-manifest.c85ee182d5024e24b699b1da6ba1cbee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d00fea6010c415c09e",
+    "url": "/keeper-app/static/js/main.297dcc58.chunk.js"
+  },
+  {
+    "revision": "7491953a57bd06d42130",
+    "url": "/keeper-app/static/js/runtime~main.afea614e.js"
+  },
+  {
+    "revision": "1371cffd67c4b48c94bb",
+    "url": "/keeper-app/static/js/2.35968f0b.chunk.js"
+  },
+  {
+    "revision": "eafa3029a106c47ce71a271fc1a2b4a3",
+    "url": "/keeper-app/index.html"
+  }
+];
